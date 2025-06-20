8b83afd863dbe73f827f6a16afdfb868a624f5bc
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 06:35:26 +0000

    [automatic commit] 2025-06-20 06:35:26 UTC ;; communications error to 208.67.222.222#53: timed out

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
       SELECT wr.*, d.name AS dog_name, d.size, u.username AS owner_name
       FROM walk_requests wr
       JOIN dogs d ON wr.dog_id = d.dog_id
-      JOIN Users u ON d.owner_id = u.user_id
+      JOIN users u ON d.owner_id = u.user_id
       WHERE wr.status = 'open'
     `);
     res.json(rows);
