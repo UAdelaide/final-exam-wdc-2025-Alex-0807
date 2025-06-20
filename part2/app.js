50343878609331aaf2b533471254da70055e0187
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 11:21:46 +0000

    [automatic commit] 2025-06-20 11:21:45 UTC

--- part2/app.js
+++ part2/app.js
@@ -3,18 +3,22 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+const session = require('express-session');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+
+// 新增：配置 session
 app.use(session({
-  secret: 'dogwalk_secret',
+  secret: 'dogwalk_secret', // 建议用更安全的密钥
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: false } // 本地开发用 false，生产环境用 true
 }));
+
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
