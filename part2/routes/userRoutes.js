68e03b0e4e2873d417ac23383c58d5a8ac86ecb0
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 11:52:42 +0000

    [automatic commit] 2025-06-20 11:52:42 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -39,9 +39,10 @@ router.get('/me', (req, res) => {
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
-  console.log('Login attempt:', username, password);
-  const { username, password } = req.body; //
+  console.log('Login request body:', req.body);
 
+  const { username, password } = req.body; //
+  console.log('Login attempt:', username, password);
   try {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM users
