c27a006d23399d523747066349582673d5ad928d
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 05:09:41 +0000

    [automatic commit] 2025-06-21 05:09:40 UTC

--- part1/app.js
+++ part1/app.js
@@ -8,7 +8,7 @@ const port = 8080;
 const dbConfig = {
   host: 'localhost',
   user: 'root',
-  // password: 'jlccXHY0807!',
+  password: '',
   // database: 'dogwalks',
   // waitForConnections: true,
   // connectionLimit: 10,
